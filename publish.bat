40b6baf2c4a21e148d6da016b6fda4179e02f563
Christian Junk <christian.junk@woodplc.com>
2020-05-20 20:24:17 +0200

    Added GitHub Packages as build target.

--- publish.bat
+++ publish.bat
@@ -6,3 +6,6 @@ if exist *.nupkg del *.nupkg
 
 REM Generate NuGet package
 %cd%\tools\nuget.exe pack FluentNHibernate.AspNetCore.Identity.nuspec
+
+REM Push to GutHub Packages
+dotnet nuget push *.nupkg --source "github"
